3a37ed05b1ff020d3b04a333769e4b54c0e5a412
BrenoLudgero <breno.ludgero@yahoo.com>
2024-11-20 15:21:56 -0300

    Add instructions, label to interface

--- config.py
+++ config.py
@@ -17,3 +17,17 @@ def create_records_folder():
     records_folder = get_records_folder_path()
     if not os.path.isdir(records_folder):
         os.makedirs(records_folder)
+
+INSTRUCTIONS = """HOW TO USE:
+
+Insert the link to each page you want to extract the recors from in Links List. For example:
+
+www.twingalaxies.com/game/tetris
+www.twingalaxies.com/game/galaga/arcade
+
+Alternatively, you may insert just the values that follow "game/":
+
+tetris
+galaga/arcade
+
+Each link must be in a new line"""
--- interface.py
+++ interface.py
@@ -1,4 +1,5 @@
 import customtkinter as ctk
+from config import INSTRUCTIONS
 
 ctk.set_default_color_theme("dark-blue")
 
@@ -8,31 +9,34 @@ class Interface(ctk.CTk):
         self.scraper = scraper
         
         # WINDOW SETUP
-        self.window_width = 1000
-        self.window_height = 800
-        self.min_window_width = 800
-        self.min_window_height = 600
+        self.window_width = 800
+        self.window_height = 600
         self.title("Twin Galaxies Record Scraper")
         self.geometry(self.get_centered_window_coords())
-        self.minsize(self.min_window_width, self.min_window_height)
-        self.columnconfigure((0, 1), weight=8)
-        self.columnconfigure(2, weight=2)
+        self.minsize(self.window_width, self.window_height)
+        self.columnconfigure((0, 1), weight=6)
+        self.columnconfigure(2, weight=4)
         self.rowconfigure(0, weight=6)
         self.rowconfigure(1, weight=1)
         self.rowconfigure(2, weight=3)
 
         # ELEMENTS
-        self.url_list_textbox = ctk.CTkTextbox(master=self, corner_radius=4, border_width=2, wrap="word")
+        self.url_list_textbox = ctk.CTkTextbox(self, corner_radius=4, border_width=2, wrap="word")
         self.url_list_textbox.grid(row=0, column=0, rowspan=2, columnspan=2, sticky="nwes", padx=10, pady=(26, 10))
 
-        self.frame = ctk.CTkFrame(master=self, border_width=1, fg_color="white")
-        self.frame.grid(row=0, column=2, sticky="news", padx=(3, 10), pady=(10, 0))
+        self.url_list_label = ctk.CTkLabel(self, text="Links List", height=1)
+        self.url_list_label.grid(row=0, column=0, columnspan=2, sticky="nwe", pady=(6, 0))
 
-        self.button = ctk.CTkButton(master=self, text="Start", command=self.start_scraping_process)
-        self.button.grid(row=1, column=2, sticky="news", padx=(3, 10), pady=10)
+        self.textbox = ctk.CTkTextbox(self, border_width=1, wrap="word")
+        self.textbox.insert("1.0", INSTRUCTIONS)
+        self.textbox.configure(state="disabled")
+        self.textbox.grid(row=0, column=2, sticky="nwes", padx=(3, 10), pady=(10, 0))
 
-        self.logs_textbox = ctk.CTkTextbox(master=self, corner_radius=0, border_width=1, fg_color="lightgray", state="disabled")
-        self.logs_textbox.grid(row=2, column=0, columnspan=3, sticky="news", padx=10, pady=(0, 10))
+        self.button = ctk.CTkButton(self, text="Start", command=self.start_scraping_process)
+        self.button.grid(row=1, column=2, sticky="nwes", padx=(3, 10), pady=10)
+
+        self.logs_textbox = ctk.CTkTextbox(self, corner_radius=0, border_width=1, fg_color="lightgray", state="disabled")
+        self.logs_textbox.grid(row=2, column=0, columnspan=3, sticky="nwes", padx=10, pady=(0, 10))
 
     def get_centered_window_coords(self):
         screen_width = self.winfo_screenwidth()
